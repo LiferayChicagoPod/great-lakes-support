d37b3e9799be1e9172c326455c4fae2cfddd5295
Isaac <isaac.wilson@liferay.com>
2023-08-30 11:27:52 -0500

    8/30 - Adding initial TimeTracker files

---
+++ TimeTracker/src/main/CPTimeTracker.java
@@ -0,0 +1,1 @@
+# This will be the coordinator file, where we call all other files for the project.
---
+++ TimeTracker/src/main/TimeTrackerFunctions.java
@@ -0,0 +1,1 @@
+# This is where we'll define the functions and other actions for the TimeTracker project.
