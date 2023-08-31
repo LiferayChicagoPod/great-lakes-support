ebd118d1eb0c53b1f49a3209bbf042b9b75e5ba8
Ethan <ethan.adelsman@liferay.com>
2023-08-31 13:21:12 -0500

    Ethan's changes to Functions

--- TimeTracker/src/main/TimeTrackerFunctions.java
+++ TimeTracker/src/main/TimeTrackerFunctions.java
@@ -1,1 +1,18 @@
-# This is where we'll define the functions and other actions for the TimeTracker project.
+ //This is where we'll define the functions and other actions for the TimeTracker project.
+ 
+TimeTrackerFunctions.java
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CPTimeStamp {
+	
+	// 2023.08.30.12.14-0700
+	private static final SimpleDateFormat dateAndTimeStamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+
+	public static void main (String[] args) {
+
+	Date x = new Date();
+	}
+}
