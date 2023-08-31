51b1bfa11929fd6aa9132841af6dd3a0aa12cb37
Ethan <ethan.adelsman@liferay.com>
2023-08-31 13:57:26 -0500

    Updated functions to Instant

--- TimeTracker/src/main/TimeTrackerFunctions.java
+++ TimeTracker/src/main/TimeTrackerFunctions.java
@@ -1,18 +1,23 @@
  //This is where we'll define the functions and other actions for the TimeTracker project.
  
-TimeTrackerFunctions.java
+//CPTimeStamp.java
 
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.Instant;
 
 public class CPTimeStamp {
 	
 	// 2023.08.30.12.14-0700
-	private static final SimpleDateFormat dateAndTimeStamp = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+	//private static final SimpleDateFormat dateAndTimeStamp = new //SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
 
 	public static void main (String[] args) {
 
-	Date x = new Date();
+	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	System.out.println(timestamp);
+	System.out.println(timestamp.getTime()); 
+
+	Instant instant = timestamp.toInstant();
+	System.out.println(instant);
+
 	}
 }
