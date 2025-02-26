f786774b1e9fccd376a7fbd72776abf96762b67e
madeleineclay <madeleine.clay@liferay.com>
2025-02-26 17:12:02 -0600

    proposed changes to Tools.java

--- Minesweeper/src/Games/MinesweeperTerminal.java
+++ Minesweeper/src/Games/MinesweeperTerminal.java
@@ -3,6 +3,23 @@ package Games;
 public class MinesweeperTerminal {
 
 	public static void createBoard() {
-		System.out.print("Dummy method");
+		System.out.print("Dummy method - you started a game of Minesweeper!");
+
+		System.out.println("How many rows would you like?");
+		int rows = Tools.getWholeNumberInput();
+
+		System.out.println("Great! We'll make " + rows + " rows");
+
+		System.out.println("How many columns would you like?");
+		int columns = Tools.getWholeNumberInput();
+
+		System.out.println("Great! We'll make " + columns + " columns");
+
+		System.out.println("How many rows would you like?");
+		int mines = Tools.getWholeNumberInput();
+
+		System.out.println("Great! We'll make " + mines + " mines");
+
+		System.out.println("\nThanks for playing! \nSelect another game?");
 	}
 }
